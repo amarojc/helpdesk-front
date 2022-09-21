e68ecb2e1605c971d8b6c4cae3ec91dbd7cf2aed
amarojc <inf.amaro.jc@gmail.com>
2022-09-20 21:21:04 -0300

    Validação de formulários para login

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,6 @@
+import { Credenciais } from './../../models/credenciais';
 import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +8,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  //Variável do cred do tipo Credenciais, com atributos email e senha
+  creds: Credenciais = {
+    email: '',
+    senha: ''
+  }
 
+  //Valida se o valor digitado da variável está no formato de email.
+  email = new FormControl(null, Validators.email);
+  //Valida se o valor digitado é maior que 3 caracteres, caso menor fica invalida.
+  senha = new FormControl(null, Validators.minLength(3));
+
+  
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  validaCampos(): boolean{
+    if(this.email.valid && this.senha.valid){
+      return true;
+    }else{
+      return false;
+    }
+  }
 }
